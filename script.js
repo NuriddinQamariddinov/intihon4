9756c3c9b843ac09c020a5e77dbb2fdf0a590fa6
NuriddinQamariddinov <146547947+NuriddinQamariddinov@users.noreply.github.com>
2023-10-13 13:15:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function hid1() {
+  document.querySelector(".hidden-section").classList.remove("sectionhid");
+}
+
+function hid2() {
+  document.querySelector(".hidden-section").classList.add("sectionhid");
+}
+
+function hid3() {
+  document.querySelector(".hidden-section2").classList.remove("sectionhid");
+}
+
+function hid4() {
+  document.querySelector(".hidden-section2").classList.add("sectionhid");
+}
+function hid5() {
+  document.querySelector(".hidden-section3").classList.remove("sectionhid");
+}
+function hid6() {
+  document.querySelector(".hidden-section3").classList.add("sectionhid");
+}
+function showText() {
+  document.querySelector(".inside-text ").classList.add("show");
+}
+function hideText() {
+  document.querySelector(".inside-text ").classList.remove("show");
+}
